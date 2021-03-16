05514f994edb0b1623bb8e411327b12d5eafcd31
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-03-16 12:34:11 +0100

    [FIX] Adjust max log entry size
    Our test framework can now handle longer entry size thus we can
    increase it in our tester.
    
    As file name path is dynamic, max log entry must be created on fly as
    well.
    
    Fixes:
    SEOS-2465

--- components/log_clients/LogsAllLevels/LogsAllLevels.c
+++ components/log_clients/LogsAllLevels/LogsAllLevels.c
@@ -1,18 +1,30 @@
 #include "lib_debug/Debug.h"
 #include <camkes.h>
 
+#include <string.h>
+
 static OS_LoggerFilter_Handle_t filter;
 
 static void testLogging();
 static void tearDownLogging();
 
-// TODO What is the max possible entry, as what's below seems to be the limit,
-//  however nothing gets displayed when used.
+// Max possible entry is 3941 characters (PAGE_SIZE - log_header
+// e.g. 4096 - 193).
 //
-// Max possible entry is:
-// (DATABUFFER_SIZE - (OS_Logger_LOG_LEVEL_LENGTH + OS_Logger_LOG_LEVEL_LENGTH))
-// == 4096 - (2 + 2)
-const char maxPossibleLogEntry[];
+// Currently log_header's size is 193, and it consists of:
+//
+// 1. client id (11 characters) e.g. `0000051966 `
+// 2. client name (15 characters) e.g. `FILTER_NULL    `
+// 3. timestamp (20 characters) e.g. `01.01.1969-00:00:00 `
+// 4. client filter_lvl (4 characters) e.g. `  0 `
+// 5. server filer_lvl (4 characters) e.g. `  0 `
+// 6. log lvl (9 characters) e.g. ` ASSERT: `
+// 7. file name (126 characters) e.g.
+//      `/home/jenkins/workspace/generic_jobs/generic_pipeline_sandbox/src/
+//          system/components/log_clients/LogsAllLevels/LogsAllLevels.c:`
+// 8. file line (4 characters including trailing whitespace) e.g. `43: `
+#define LOG_HEADER_SZ (11 + 15 + 20 + 4 + 4 + sizeof(__FILE__) + 4)
+char maxPossibleLogEntry[PAGE_SIZE - LOG_HEADER_SZ];
 
 const char format_string[] =
                     "%d %u %o %x %X %f %F %e %E %g %G %a %A %c %s %p %n %%";
@@ -30,6 +42,8 @@ void post_init()
         OS_LoggerFilter_ctor(&filter, log_lvl);
         OS_LoggerEmitter_getInstance(logServer_buf, &filter, API_LOG_SERVER_EMIT);
     }
+
+    memset(maxPossibleLogEntry, 'H', sizeof(maxPossibleLogEntry));
 }
 
 int run()
@@ -64,35 +78,3 @@ void tearDownLogging()
 {
     done_emit();
 }
-
-const char maxPossibleLogEntry[] =
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit nulla imperdiet "
-    "tempus, class litora sagittis per vitae varius risus potenti platea, "
-    "magnis pellentesque tristique nec volutpat lacinia ultrices conubia "
-    "praesent. Sociis justo tempus turpis in bibendum volutpat sodales "
-    "malesuada nisl magnis primis etiam vitae pellentesque potenti, aliquam "
-    "torquent eu ante dignissim duis tincidunt congue tellus laoreet morbi "
-    "feugiat class. Cum sem taciti mattis iaculis fringilla accumsan nec, quam "
-    "purus cubilia nisl dictumst ad dis nulla, tempor egestas eu habitant "
-    "massa justo. Sem fringilla iaculis rutrum malesuada litora auctor congue "
-    "faucibus, ligula platea hendrerit tempor est aliquet integer, laoreet "
-    "potenti nisl augue semper lacus lobortis. Nisl iaculis magna lectus odio "
-    "facilisis est nullam, feugiat varius nunc nibh mi primis scelerisque "
-    "conubia, cras nam malesuada tellus gravida diam. Ut habitasse quis "
-    "rutrum venenatis sagittis dignissim orci blandit pretium ad magna, "
-    "eleifend malesuada purus condimentum posuere sollicitudin eget "
-    "ullamcorper tempor. Nam nostra nulla suscipit pharetra purus sociis "
-    "molestie fames primis interdum dapibus, tempus ante justo tellus orci "
-    "pretium sed dis ac urna id, accumsan habitasse lacinia tincidunt sociosqu "
-    "sagittis netus euismod varius venenatis. Metus urna tempor primis massa "
-    "porta libero auctor sem in tellus, nunc velit orci viverra elementum "
-    "sociis ridiculus eget iaculis litora convallis, fusce quam vitae lacinia "
-    "est taciti hac luctus tempus. Rhoncus litora mattis mus torquent fusce "
-    "justo elementum fermentum, per tempor imperdiet facilisi conubia lacinia "
-    "sociosqu, eros in mollis enim dis neque blandit. Posuere purus odio "
-    "bibendum sagittis nisl metus, lobortis commodo mi ante urna. Congue "
-    "aliquet felis hac euismod id, class viverra sociis massa in egestas, "
-    "litora magna commodo ornare. Euismod in metus sociis pellentesque aenean "
-    "blandit, cubilia nibh eu pharetra hendrerit parturient purus, nisl "
-    "viverra pra.";
-
