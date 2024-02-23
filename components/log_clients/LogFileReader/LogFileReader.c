475405695122949f185ddb149fc3ceb636915d31
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-23 10:12:02 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/log_clients/LogFileReader/LogFileReader.c
+++ components/log_clients/LogFileReader/LogFileReader.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2020-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
+ */
+
 #include "lib_debug/Debug.h"
 #include "Logger/Client/OS_LoggerFileClient.h"
 #include <camkes.h>
--- components/log_clients/LogsAllLevels/LogsAllLevels.c
+++ components/log_clients/LogsAllLevels/LogsAllLevels.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2020-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
+ */
+
 #include "lib_debug/Debug.h"
 #include "lib_compiler/compiler.h"
 #include "Logger/Common/OS_LoggerEntry.h"
